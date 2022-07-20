89a1a1066feaf7de8b67d5caa5b858a9804490da
chhetri28 <chettrisourav28@gmail.com>
2022-07-20 10:04:40 +0530

    4

--- 2 pointers/Boats to Save People.cpp
+++ 2 pointers/Boats to Save People.cpp
@@ -1,13 +1,18 @@
-class Solution {
+class Solution
+{
 public:
-    int numRescueBoats(vector<int>& people, int limit) {
-        int i=0;
-        int j=people.size()-1;
-        sort(people.begin(),people.end());
-        int boats=0;
-        while(i<=j){
-            if(people[i]+people[j]<=limit) i++,j--;
-            else j--;
+    int numRescueBoats(vector<int> &people, int limit)
+    {
+        int i = 0;
+        int j = people.size() - 1;
+        sort(people.begin(), people.end());
+        int boats = 0;
+        while (i <= j)
+        {
+            if (people[i] + people[j] <= limit)
+                i++, j--;
+            else
+                j--;
             boats++;
         }
         return boats;
