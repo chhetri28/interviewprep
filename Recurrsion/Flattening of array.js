bc898b821e2b39eceae73db25326d6bfc955a219
chhetri28 <chettrisourav28@gmail.com>
2022-07-29 22:51:54 +0530

    6

---
+++ Recurrsion/Flattening of array.js
@@ -0,0 +1,15 @@
+let aa=[1,2,3,[[[4]]],[5]];
+
+const flatten=(arr)=>{
+    res=arr.reduce((acc,item)=>{
+        if(Array.isArray(item)){
+            acc=acc.concat(flatten(item));
+        }
+        else acc.push(item);
+
+        return acc;
+    },[]);
+    return res;
+}
+
+console.log(flatten(aa));
